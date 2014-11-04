64f4b93cf66bedce88b4abf3a28aaec319e2fe02
Somnid <>
2014-11-04 10:21:55 -0700

    additions to path-tools

--- path-tools.js
+++ path-tools.js
@@ -5,6 +5,13 @@ var pathTools = (function(){
 		return split[split.length - 1];
 	}
 
+	function getFilenameNoExtension(path){
+		var split = path.split("/");
+		var filename = split[split.length - 1];
+		var filenameSplit = filename.split(".");
+		return filenameSplit[0];
+	}
+
 	function getExtension(path){
 		var split = path.split(".");
 		return split[split.length - 1];
@@ -18,11 +25,27 @@ var pathTools = (function(){
 	  return path.indexOf(":") != -1;
 	}
 
+	function removePreceedingSlash(path){
+    return path[0] == "/" ?  path.substr(1) : path;
+  }
+
+  function removeQueryString(path){
+    return path.split("?")[0];
+  }
+
+  function removeHash(path){
+    return path.split("#")[0];
+  }
+
 	return {
 		getFilename : getFilename,
+		getFilenameNoExtension : getFilenameNoExtension,
 		getExtension : getExtension,
 		getParentDirectory : getParentDirectory,
-		isAbsolute : isAbsolute
+		isAbsolute : isAbsolute,
+		removePreceedingSlash : removePreceedingSlash,
+		removeQueryString : removeQueryString,
+		removeHash : removeHash
 	};
 
 })();
--- tests/path-tools/test.js
+++ tests/path-tools/test.js
@@ -1,4 +1,5 @@
-test("pathTools gets parentDirectory", function(){
+module(".parentDirectory");
+test("gets parentDirectory", function(){
 	var parent = pathTools.getParentDirectory("path/to/file.ext");
 	equal(parent, "path/to", "local file");
 
@@ -6,7 +7,8 @@ test("pathTools gets parentDirectory", function(){
 	equal(parent2, "http://www.website.com/path/path2/path3", "web url");
 });
 
-test("pathTools finds if absolute", function(){
+module(".isAbsolute")
+test("determines if absolute", function(){
 	var test1 = pathTools.isAbsolute("path/to/file.ext");
 	equal(test1, false, "relative local file");
 
@@ -18,4 +20,22 @@ test("pathTools finds if absolute", function(){
 
 	var test4 = pathTools.isAbsolute("./path/path2/path3/file.ext");
 	equal(test4, false, "relative web url");
+});
+
+module(".getFilename");
+test("gets the filename", function(){
+	var test1 = pathTools.getFilename("path/to/file.ext");
+	equal(test1, "file.ext");
+
+	var test2 = pathTools.getFilename("C:/path/to/file2.ext");
+	equal(test2, "file2.ext");
+});
+
+module(".getFilenameNoExtension");
+test("gets the filename", function(){
+	var test1 = pathTools.getFilenameNoExtension("path/to/file.ext");
+	equal(test1, "file");
+
+	var test2 = pathTools.getFilenameNoExtension("C:/path/to/file2.ext");
+	equal(test2, "file2");
 });
