ffcb4a2f2ca3cc445a7c288d8e556ce1723bf9e3
ndesmic <ndesmic@duck.com>
2024-01-01 20:36:40 -0800

    getPermutation of size

--- libs/combinatorics-tools.js
+++ libs/combinatorics-tools.js
@@ -1,6 +1,7 @@
-export function getPermutations(items){
-	if(items.length === 0) return [];
-	if(items.length === 1) return [items];
+export function getPermutations(items, size){
+	size ??= items.length;
+	if(size > items.length) return [];
+	if(size === 1) return items.map(x => [x]);
 	return items.flatMap(x => 
-		getPermutations(items.filter(v => x != v)).map(vs => [x, ...vs]));
+		getPermutations(items.filter(v => x !== v), size - 1).map(item => [x, ...item]));
 }
--- tests/combinatorics-tools-test.js
+++ tests/combinatorics-tools-test.js
@@ -10,4 +10,13 @@ describe("getPermutations", () => {
 		expect(result[4]).toEqual([3, 1, 2]);
 		expect(result[5]).toEqual([3, 2, 1]);
 	});
+	it("gets all permutations of size", () => {
+		const result = getPermutations([1, 2, 3], 2);
+		expect(result[0]).toEqual([1, 2]);
+		expect(result[1]).toEqual([1, 3]);
+		expect(result[2]).toEqual([2, 1]);
+		expect(result[3]).toEqual([2, 3]);
+		expect(result[4]).toEqual([3, 1]);
+		expect(result[5]).toEqual([3, 2]);
+	});
 });
